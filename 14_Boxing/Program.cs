66687035c61143992470841e6721a7bb60083978
ivane <ivaneremov@icloud.com>
2020-10-01 21:13:51 +0300

    01.10.2020

---
+++ 14_Boxing/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _14_Boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short a = 25;
+
+            object o = a;
+
+            short b = (short)o;
+            Console.WriteLine(b);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 23_Structure/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _23_Structure
+{
+
+    struct MyStruct
+    {
+        public int field;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStruct myStruct;
+            myStruct.field = 5;
+            Console.WriteLine(myStruct.field);
+            Console.ReadLine();
+        }
+    }
+}
