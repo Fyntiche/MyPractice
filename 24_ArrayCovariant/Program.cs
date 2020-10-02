d0662425223e1a5f7e9e69d70af30610f89affa1
Ivan <ivaneremov@icloud.com>
2020-10-02 17:35:17 +0300

    02.10.2020

---
+++ 24_ArrayCovariant/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _24_ArrayCovariant
+{
+
+    interface IVoice
+    {
+        void Voice();
+    }
+
+    public struct Dog : IVoice
+    {
+        public void Voice()
+        {
+            Console.WriteLine("Voice");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog[] dogs = { new Dog(), new Dog(), new Dog() };
+
+           // IVoice[] animal = dogs; Error
+        }
+    }
+}
---
+++ 25_Enums/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _25_Enums
+{
+    enum EnumType : byte
+    {
+        one,
+        two,
+        three
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(EnumType.one);
+            Console.WriteLine((byte)EnumType.one);
+            for (EnumType number = EnumType.one; number < EnumType.three; number++)
+            {
+                Console.WriteLine((byte)number);
+            }
+            Type @enum = EnumType.one.GetType();
+            Console.WriteLine(@enum.Name);
+            Console.WriteLine(@enum.FullName);
+            Console.WriteLine(Enum.GetUnderlyingType(@enum));
+            Console.Read();
+        }
+    }
+}
