d1a8e288975b7fd5dd2e9a2d285626f1f56af369
Ivan <ivaneremov@icloud.com>
2020-10-05 18:04:23 +0300

    05.10.2020

---
+++ 39_Constraint/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _39_Constraint
+{
+
+    class MyClass<T> where T : struct
+    {
+        public T variable;
+    }
+
+    class MyClass1<T> where T: class
+    {
+        public T variable;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass<int> myClass = new MyClass<int>();
+            Console.WriteLine(myClass.variable.GetType());
+            MyClass1<string> myClass1 = new MyClass1<string>();
+            myClass1.variable = "1";
+            Console.WriteLine(myClass1.variable.GetType());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 40_NewConstraint/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _40_NewConstraint
+{
+    class MyClass<T> where T : new()
+    {
+        public T variable = new T();
+
+        public new void GetType()
+        {
+            Console.WriteLine(variable.ToString());
+        }
+    }
+
+    class MyTest
+    {
+        public int MyVariable = 1;
+        public int MyVariable1 = 2;
+
+        public override string ToString()
+        {
+            return string.Format("{0} - {1}", MyVariable1, MyVariable);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass<MyTest> myClass = new MyClass<MyTest>();
+            myClass.GetType();
+            Console.ReadLine();
+        }
+    }
+}
