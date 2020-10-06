504d0185c446d5d70e312a0ca1e35fa9c435b270
ivane <ivaneremov@icloud.com>
2020-10-06 21:26:51 +0300

    06.10.2020 part 2

---
+++ 43_Events/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace _43_Events
+{
+    public delegate void EventDelegate();
+
+    internal class Keyboard
+    {
+        private EventDelegate aPressKey;
+        private EventDelegate bPressKey;
+
+        public event EventDelegate APressKey
+        {
+            add
+            {
+                aPressKey += value;
+            }
+            remove
+            {
+                aPressKey -= value;
+            }
+        }
+
+        public event EventDelegate BPressKey
+        {
+            add
+            {
+                bPressKey += value;
+            }
+            remove
+            {
+                bPressKey -= value;
+            }
+        }
+
+        public void APressKeyInvoke()
+        {
+            if (aPressKey != null)
+            {
+                aPressKey.Invoke();
+            }
+        }
+
+        public void BPressKeyInvoke()
+        {
+            if (bPressKey != null)
+            {
+                bPressKey.Invoke();
+            }
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                string key = Console.ReadLine();
+                switch (key)
+                {
+                    case "a":
+                    case "A":
+                        APressKeyInvoke();
+                        continue;
+                    case "b":
+                    case "B":
+                        BPressKeyInvoke();
+                        continue;
+                    case "exit":
+                        break;
+                }
+            }
+        }
+    }
+
+    internal class Program
+    {
+        private static void DrawAImage()
+        {
+            Console.WriteLine("a");
+        }
+
+        private static void DrawBImage()
+        {
+            Console.WriteLine("b");
+        }
+
+        private static void Main(string[] args)
+        {
+            Keyboard keyboard = new Keyboard();
+            keyboard.APressKey += DrawAImage;
+            keyboard.BPressKey += DrawBImage;
+            keyboard.Run();
+        }
+    }
+}
---
+++ 44_Events/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace _44_Events
+{
+    public delegate void MyDelegate();
+    interface EventInteface
+    {
+        event MyDelegate MyEvent;
+    }
+
+    class BaseClass : EventInteface
+    {
+        private MyDelegate eventDelegate = null;
+
+        public virtual event MyDelegate MyEvent
+        {
+            add
+            {
+                eventDelegate += value;
+            }
+            remove
+            {
+                eventDelegate -= value;
+            }
+        }
+        public void MyEventInvoke()
+        {
+            eventDelegate.Invoke();
+        }
+    }
+
+    class DerivedClass : BaseClass
+    {
+        public override event MyDelegate MyEvent
+        {
+            add
+            {
+                base.MyEvent += value;
+                Console.WriteLine("DerivedClass");
+            }
+
+            remove
+            {
+                base.MyEvent -= value;
+                Console.WriteLine("DerivedClass");
+            }
+        }
+    }
+    class Program
+    {
+        public static void Handler1()
+        {
+            Console.WriteLine("Handler1");
+        }
+
+        public static void Handler2()
+        {
+            Console.WriteLine("Handler2");
+        }
+
+        static void Main(string[] args)
+        {
+            BaseClass baseClass = new BaseClass();
+            baseClass.MyEvent += Handler1;
+            baseClass.MyEvent += Handler2;
+            baseClass.MyEventInvoke();
+            Console.WriteLine(new string('_', 50));
+            DerivedClass derivedClass = new DerivedClass();
+            derivedClass.MyEvent += Handler2;
+            derivedClass.MyEventInvoke();
+            Console.WriteLine(new string('_', 50));
+            DerivedClass derivedClass1 = new DerivedClass();
+            derivedClass1.MyEvent += Handler1;
+            derivedClass1.MyEvent += Handler2;
+            derivedClass1.MyEventInvoke();
+            Console.ReadKey();
+        }
+    }
+}
