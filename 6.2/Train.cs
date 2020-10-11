a8b7390d96013b6d24f37e0028700d7c0b52df04
ivane <ivaneremov@icloud.com>
2020-10-11 22:35:45 +0300

    11.10.2020

---
+++ 6.2/Train.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _6._2
+{
+    internal struct Train
+    {
+        public readonly string From { get; }
+
+        public readonly int Number { get; }
+
+        public readonly DateTime Time { get; }
+
+        public Train(string from, int number, DateTime time)
+        {
+            From = from;
+            Number = number;
+            Time = time;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("From = {0}, number = {1}, Time = {2}", From, Number, Time);
+        }
+    }
+}
---
+++ 8.0/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _8._0
+{
+    internal class Program
+    {
+        private delegate void Sum(int a, int b, int c);
+
+        private static void Main(string[] args)
+        {
+            Sum srt = null;
+            srt += Add;
+            srt += (a, b, c) => Console.WriteLine(a + b + c);
+            srt(1, 2, 3);
+            Console.ReadKey();
+        }
+
+        private static void Add(int a, int b, int c)
+        {
+            Console.WriteLine((a + b + c) / 3);
+        }
+    }
+}
