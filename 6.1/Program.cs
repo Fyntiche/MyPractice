73dbb64668855a6ea9364c9ddc4232a290584c24
Ivan <ivaneremov@icloud.com>
2020-10-12 09:15:18 +0300

    12.10.2020

---
+++ 6.1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _6._1
+{
+    internal class Program
+    {
+        public struct Notebook
+        {
+            public string Model;
+
+            public string Made;
+
+            public double Price;
+
+            public Notebook(string model, string made, double price)
+            {
+                Model = model;
+                Made = made;
+                Price = price;
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            Notebook notebook = new Notebook("Phone", "Apple", 12);
+            Console.WriteLine(notebook.Made + " " + notebook.Model + " " + notebook.Price);
+            Console.ReadLine();
+        }
+    }
+}
